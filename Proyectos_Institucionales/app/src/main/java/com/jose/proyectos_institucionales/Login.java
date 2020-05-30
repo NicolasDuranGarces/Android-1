c8b3ac2cf72dd15b91bd4f471813a8821ad122b6
papugarces <nicolas.duran.1721@eam.edu.co>
2020-05-30 17:01:01 -0500

    Correcion Pos De Imágenes con LinearVertical

--- Proyectos_Institucionales/app/src/main/java/com/jose/proyectos_institucionales/Login.java
+++ Proyectos_Institucionales/app/src/main/java/com/jose/proyectos_institucionales/Login.java
@@ -2,7 +2,9 @@ package com.jose.proyectos_institucionales;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void regresar(View view){
+        Intent intent = new Intent(this , MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void iniciar(View view){
+        Intent intent = new Intent(this , Login.class);
+        startActivity(intent);
     }
 }
